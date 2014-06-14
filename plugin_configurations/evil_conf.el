a1aa4c9e51bdd71f883e55809fdbcf1662e2993f
Apple <Apple@server.fake>
2014-06-14 22:11:26 +0100

    Overrided some evil mappings to allow for emacs bindings

--- plugin_configurations/evil_conf.el
+++ plugin_configurations/evil_conf.el
@@ -10,3 +10,19 @@
 
 (my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
 (my-move-key evil-normal-state-map evil-normal-state-map " ")
+
+;; Remove some of the scrolling commands in evil to leave the default emacs ones
+(define-key evil-motion-state-map (kbd "C-e") nil)
+(define-key evil-motion-state-map (kbd "C-y") nil)
+
+;; Override the prefix key to allow the scrolling.
+;; (define-key evil-motion-state-map (kbd "C-u") 'evil-scroll-up)
+
+;; Change the insert mode mappings to reflect the fact I'm in emacs
+(define-key evil-insert-state-map (kbd "C-k") nil)
+(define-key evil-insert-state-map (kbd "C-a") nil)
+(define-key evil-insert-state-map (kbd "C-e") nil)
+(define-key evil-insert-state-map (kbd "C-y") nil)
+;; (define-key evil-insert-state-map "  " 'execute-extended-command)
+
+(define-key evil-insert-state-map (kbd "RET") nil)
