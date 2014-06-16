b9b70eefb05e239e1f15351fbd5ff0d7adbe25c6
Apple <Apple@server.fake>
2014-06-16 18:44:20 +0100

    Made evil-insert-mode back to default.
    I had previously removed some mappings from evil-insert-mode, now
    putting them back as I swap between evil-mode and emacs-mode more.

--- plugin_configurations/evil_conf.el
+++ plugin_configurations/evil_conf.el
@@ -16,28 +16,14 @@
   (interactive)
   (evil-yank (point) (point-at-eol)))
 
-;; Override the prefix key to allow the scrolling.
 (define-key evil-normal-state-map "Y" 'copy-to-end-of-line)
+
+;; Override the prefix key to allow the scrolling.
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 
 ;; Remove some of the scrolling commands in evil to leave the default emacs ones
-(define-key evil-motion-state-map (kbd "C-e") nil)
-(define-key evil-motion-state-map (kbd "C-y") nil)
 (define-key evil-motion-state-map (kbd "'") 'evil-goto-mark)
 (define-key evil-motion-state-map (kbd "`") 'evil-goto-mark-line)
-
-;; Change the insert mode mappings to reflect the fact I'm in emacs
-;; Not sure about overriding evil's completion mappings
-;; Am doing it so I have basically emacs when in insert mode
-(setcdr evil-insert-state-map nil)
-(define-key evil-insert-state-map (kbd "C-r") 'evil-paste-from-register)
-(define-key evil-insert-state-map (kbd "C-w") 'evil-delete-backward-word)
-(define-key evil-insert-state-map (kbd "C-z") 'evil-emacs-state)
-(define-key evil-insert-state-map (kbd "<escape>") 'evil-normal-state)
-(define-key evil-insert-state-map (kbd "<mouse-2>") 'evil-normal-state)
-(define-key evil-insert-state-map (kbd "C-x C-n") 'evil-complete-next-line)
-(define-key evil-insert-state-map (kbd "C-x C-p") 'evil-complete-previous-line)
-;; (define-key evil-insert-state-map "  " 'execute-extended-command)
 
 
 ;; All below will be moved into an "evil-unimpaired.el" plugin with extras.
