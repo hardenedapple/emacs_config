fa6376e61086552e04a27876d3336040155eb276
Apple <Apple@server.fake>
2014-06-21 19:20:11 +0100

    Added configuration for emacs calendar

---
+++ plugin_configurations/89_calendar_and_diary_conf.el
@@ -0,0 +1,9 @@
+;;; Settings regarding organisation
+(setq calendar-week-start-day 1)
+
+(setq calendar-latitude 54)
+(setq calendar-longitude 0)
+
+;;; Diary settings
+(setq diary-file "~/.emacs.d/diary")
+(calendar-set-date-style 'european)
