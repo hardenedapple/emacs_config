f8adbd4b30e4a48131ee53c993b36a0d5513f2cf
Apple <Apple@server.fake>
2014-06-22 22:32:20 +0100

    Added ido quick move to home.
    Taken from http://whattheemacsd.com/

--- plugin_configurations/89_ido_conf.el
+++ plugin_configurations/89_ido_conf.el
@@ -1,2 +1,14 @@
 ;; ido mode - more options for selecting buffers/files
 (ido-mode t)
+
+;;; Go straight to home
+(add-hook 'ido-setup-hook
+ (lambda ()
+   ;; Go straight home
+   (define-key ido-file-completion-map
+     (kbd "~")
+     (lambda ()
+       (interactive)
+       (if (looking-back "/")
+           (insert "~/")
+         (call-interactively 'self-insert-command))))))
