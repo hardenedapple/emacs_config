0f41a88f4e0c88a14aaa0ad3a087cd0304b3991e
Apple <Apple@server.fake>
2014-06-22 17:00:03 +0100

    Ediff ALWAYS vertical split

--- plugin_configurations/89_ediff_conf.el
+++ plugin_configurations/89_ediff_conf.el
@@ -1,4 +1,2 @@
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
-(setq ediff-split-window-function (if (> (frame-width) 150 )
-                                      'split-window-horizontally
-                                    'split-window-vertically))
+(setq ediff-split-window-function 'split-window-vertically)
