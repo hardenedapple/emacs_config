5171000d650b3646e7cabc0d5bbfb4ba0865080e
Apple <Apple@server.fake>
2014-06-22 16:16:11 +0100

    Added evil leader functions for magit.

--- plugin_configurations/89_magit_conf.el
+++ plugin_configurations/89_magit_conf.el
@@ -1,3 +1,15 @@
 ;; Configuration for magit
 (setq magit-repo-dirs '("~/.emacs.d" "~/share/repos/useful-files"))
+(defun magit-stage-this-file ()
+  "Stage the file the current buffer is visiting."
+  (interactive)
+  (magit-stage-item (buffer-file-name)))
 
+;;; Set some keys for evil
+(evil-leader/set-key
+  "gg" 'magit-log
+  "gw" 'magit-stage-this-file
+  "gd" 'magit-diff-unstaged
+  "gp" 'magit-push
+  "gf" 'magit-pull
+  "gs" 'magit-status)
