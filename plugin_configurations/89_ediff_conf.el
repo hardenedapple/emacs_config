10ecc4ef2ca63b6b15d0555d394611f00280e1ec
Apple <Apple@server.fake>
2014-06-22 21:40:46 +0100

    Had ediff split the wrong way.

--- plugin_configurations/89_ediff_conf.el
+++ plugin_configurations/89_ediff_conf.el
@@ -1,2 +1,2 @@
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
-(setq ediff-split-window-function 'split-window-vertically)
+(setq ediff-split-window-function 'split-window-horizontally)
