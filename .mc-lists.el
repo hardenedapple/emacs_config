bd2ea6e20d77d9e32fad09f2759ec2a4051ae70c
Apple <Apple@server.fake>
2014-06-23 11:51:45 +0100

    Allow more functions in multiple cursors

--- .mc-lists.el
+++ .mc-lists.el
@@ -2,3 +2,15 @@
 ;; It keeps track of your preferences for running commands with multiple
 ;; cursors.
 
+(setq mc/cmds-to-run-for-all
+      '(
+        eval-last-sexp
+        kill-sexp
+        paredit-backward
+        paredit-forward
+        paredit-forward-delete
+        ))
+
+(setq mc/cmds-to-run-once
+      '(
+        ))
