22b809618cb2cae2b685c1d04dffab4f817c4398
Apple <Apple@server.fake>
2014-06-23 11:36:54 +0100

    Changed key dor 5 line motion.

--- init.el
+++ init.el
@@ -129,12 +129,12 @@
 (global-set-key (kbd "RET") 'newline-and-indent)
 
 ;; Move more quickly
-(global-set-key (kbd "C-s-n")
+(global-set-key (kbd "C-S-n")
                 (lambda ()
                   (interactive)
                   (ignore-errors (next-line 5))))
 
-(global-set-key (kbd "C-s-p")
+(global-set-key (kbd "C-S-p")
                 (lambda ()
                   (interactive)
                   (ignore-errors (previous-line 5))))
