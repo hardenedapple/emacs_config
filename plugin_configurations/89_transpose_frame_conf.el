e3ea527709a24c20f697aff53902b73113fb5d55
Apple <Apple@server.fake>
2014-06-23 10:47:38 +0100

    C-x 4 maps for transpose-frame functions.

--- plugin_configurations/89_transpose_frame_conf.el
+++ plugin_configurations/89_transpose_frame_conf.el
@@ -1,2 +1,5 @@
 ;; Load transpose-frames and set keybindings
 (define-key ctl-x-4-map (kbd "t") 'transpose-frame)
+(define-key ctl-x-4-map (kbd "v") 'flip-frame)
+(define-key ctl-x-4-map (kbd "h") 'flop-frame)
+(define-key ctl-x-4-map (kbd "r") 'rotate-frame-clockwise)
