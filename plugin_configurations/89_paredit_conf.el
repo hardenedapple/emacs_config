629728c9267545ac178eb3564a2b48170826490f
Apple <Apple@server.fake>
2014-06-23 09:58:04 +0100

    Added duplicate sexp function.
    Taken from http://whattheemacsd.com/

--- plugin_configurations/89_paredit_conf.el
+++ plugin_configurations/89_paredit_conf.el
@@ -7,6 +7,33 @@
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
+(defun paredit--is-at-start-of-sexp ()
+  (and (looking-at "(\\|\\[")
+       (not (nth 3 (syntax-ppss))) ;; inside string
+       (not (nth 4 (syntax-ppss))))) ;; inside comment
+
+(defun paredit-duplicate-closest-sexp ()
+  (interactive)
+  ;; skips to start of current sexp
+  (while (not (paredit--is-at-start-of-sexp))
+    (paredit-backward))
+  (set-mark-command nil)
+  ;; while we find sexps we move forward on the line
+  (while (and (bounds-of-thing-at-point 'sexp)
+              (<= (point) (car (bounds-of-thing-at-point 'sexp)))
+              (not (= (point) (line-end-position))))
+    (forward-sexp)
+    (while (looking-at " ")
+      (forward-char)))
+  (kill-ring-save (mark) (point))
+  ;; go to the next line and copy the sexprs we encountered
+  (paredit-newline)
+  (yank)
+  (exchange-point-and-mark))
+
+;;; Add keybinding C-c d to run paredit-duplicate-closest-sexp in paredit
+(define-key paredit-mode-map (kbd "C-c d") 'paredit-duplicate-closest-sexp)
+
 ;; paredit with eldoc
 (eldoc-add-command
  'paredit-backward-delete
