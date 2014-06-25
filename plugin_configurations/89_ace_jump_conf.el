7f4603e7fd3e09c87a2e80a80585b19bb59b6b62
Apple <Apple@server.fake>
2014-06-25 22:08:56 +0100

    Use evils integration with ace-jump-mode.

--- plugin_configurations/89_ace_jump_conf.el
+++ plugin_configurations/89_ace_jump_conf.el
@@ -12,5 +12,5 @@
 (define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
 
 (evil-leader/set-key
-  "mw" 'ace-jump-mode
-  "mc" 'ace-jump-char-mode)
+  "mw" 'evil-ace-jump-mode
+  "mc" 'evil-ace-jump-char-mode)
