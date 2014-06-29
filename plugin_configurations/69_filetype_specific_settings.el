5dc6ae57334796b776632167020524045b902ff6
Apple <Apple@server.fake>
2014-06-29 18:18:57 +0100

    Turn on eldoc on emacs lisp mode.

--- plugin_configurations/69_filetype_specific_settings.el
+++ plugin_configurations/69_filetype_specific_settings.el
@@ -1,5 +1,11 @@
+;;; Elisp Settings
+;;;
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (turn-on-eldoc-mode)))
+
 ;;; Latex Settings
 ;;;
 (add-hook 'latex-mode-hook
-          '(lambda ()
-             (set-fill-column 125))) ; usually only write latex on large screens
+          (lambda ()
+            (set-fill-column 125)))  ; usually only write latex on large screens
