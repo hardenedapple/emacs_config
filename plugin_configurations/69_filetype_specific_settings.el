1c3e20366eed7a166b83895363cd1d0b760784c1
Apple <Apple@server.fake>
2014-07-01 21:09:22 +0100

    Manually add pylint support for pylint.
    Can't get the python-mode built in support to work will eventually.

--- plugin_configurations/69_filetype_specific_settings.el
+++ plugin_configurations/69_filetype_specific_settings.el
@@ -4,6 +4,7 @@
       c-basic-offset 4)
 (c-set-offset 'case-label '+)
 
+
 ;;; Latex Settings
 ;;;
 (add-hook 'latex-mode-hook
@@ -14,6 +15,25 @@
 ;;; Lisp Settings
 ;;;
 (setq inferior-lisp-program "/usr/bin/sbcl")
+
+
+;;; Python Settings
+;;;
+;; Manually set the flymake configuration
+;; At the moment the pylint flymake option in python-mode isn't doing anything
+(setq pylint "epylint")
+
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list pylint (list local-file)))))
+
+(add-hook 'flymake-allowed-file-name-masks
+          '("\\.py\\'" flymake-pylint-init))
 
 
 ;;; Script Settings
