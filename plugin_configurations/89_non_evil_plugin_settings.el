177df36cc6045b847e77c80850da21c87bd2b61a
Apple <Apple@server.fake>
2014-07-07 21:50:27 +0100

    Add C-q to call smex.
    Seeing as smex is a very frequently called command, it makes sense for it to be
    very easy to call.

--- plugin_configurations/89_non_evil_plugin_settings.el
+++ plugin_configurations/89_non_evil_plugin_settings.el
@@ -232,6 +232,7 @@
 (smex-initialize)
 
 (global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "C-q") 'smex)
 (global-set-key (kbd "<menu>") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c M-x") 'smex-update)
