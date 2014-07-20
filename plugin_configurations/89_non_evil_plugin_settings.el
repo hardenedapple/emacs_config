05fadb2d3e064c28f7b0a1524991fc4b6194b196
Apple <Apple@server.fake>
2014-07-20 09:33:44 +0100

    Disabled slime highlight edits.

--- plugin_configurations/89_non_evil_plugin_settings.el
+++ plugin_configurations/89_non_evil_plugin_settings.el
@@ -258,13 +258,13 @@
 ;;;;
 (add-to-list 'load-path "~/.emacs.d/packages/slime")
 (require 'slime-autoloads)
-(setq slime-contribs '(slime-fancy slime-highlight-edits))
+(setq slime-contribs '(slime-fancy))
 
 (define-key global-map (kbd "C-c s") 'slime-selector)
 (add-hook 'slime-mode-hook
           (lambda ()
-            (slime-highlight-edits-mode nil)
             (define-key slime-mode-map (kbd "C-c h") 'slime-highlight-edits-mode)))
+
 (setq slime-autodoc-use-multiline-p t)
 
 ;; Stop SLIME's REPL from grabbing DEL (keep balanced paranthesis)
