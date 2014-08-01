9f9377126b30c40fc69517ebcc9d75e502fa2909
Apple <Apple@server.fake>
2014-08-01 23:49:43 +0100

    Changed python.el send-region for my preferences.

--- plugin_configurations/79_built_in_packages.el
+++ plugin_configurations/79_built_in_packages.el
@@ -122,6 +122,29 @@
    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 
+;;; Remove the mavy lines sent to the python shell so the REPL looks nice
+;;; (see python.el for any updates)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (defun python-shell-send-region (start end)
+              "Send the region delimited by START and END to inferior Python process."
+              (interactive "r")
+              (save-excursion
+                (goto-char start)
+                (if (looking-at "[ \t]")
+                    (let* ((lines (split-string (buffer-substring start end) "\n" t))
+                           (initial-indent (string-match "[^ \t]" (nth 0 lines)))
+                           (newlist))
+                      (dolist (line lines)
+                        (if (> initial-indent (string-match "[^ \t]" line))
+                            (error "Indentation of region is wrong %d" initial-indent)
+                          (push (substring line initial-indent) newlist)))
+                      (python-shell-send-string
+                       (mapconcat 'identity newlist "\n") nil t))
+                  (python-shell-send-string
+                   (buffer-substring start end) nil t))))))
+
+
 ;;;; Uniquify Settings
 ;;;;
 (setq uniquify-buffer-name-style 'post-forward)
