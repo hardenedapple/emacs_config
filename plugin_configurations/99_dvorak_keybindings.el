45eb0f4a0e4a27de5f9a12bb6df2ef82ade351e7
Apple <Apple@server.fake>
2015-01-01 16:47:13 +0000

    Use KEY-TRANSLATION-MAP for Dvorak mappings
    Fuco on #emacs mentioned key-translation-map, which means I can make
    "everywhere" mappings on things like M-p <-> M-h
    
    Now use this for all the mappings reorganising bindings for my special
    keyboard.

--- plugin_configurations/99_dvorak_keybindings.el
+++ plugin_configurations/99_dvorak_keybindings.el
@@ -13,37 +13,31 @@
 ;;;;          consistency of having them remapped.
 ;;;;
 
-(global-set-key (kbd "C-S-h") (lambda (numtimes) (interactive "p")
-                                (ignore-errors (previous-line (* numtimes 5)))))
-
-;; Can't use keyboard-translate here as C-' is not a single ascii character.
-(global-set-key (kbd "C-'") ctl-x-map)
-;; Make the switch between "h" and "p" more thorough
-(global-set-key (kbd "C-M-p") 'mark-defun)
-(global-set-key (kbd "C-M-h") 'backward-list)
-(global-set-key (kbd "M-g M-h") 'previous-error)
 (global-set-key (kbd "M-g h") 'previous-error)
-(global-set-key (kbd "M-p") 'mark-paragraph)
 
-;;; Using M-f and M-b for word motion is a pain, swap with M-a and M-e
+;;; Using M-f and M-b for word motion is a pain, add M-a and M-e for word motion
 (global-set-key (kbd "M-a") 'subword-backward)
 (global-set-key (kbd "M-e") 'subword-forward)
-;; (global-set-key (kbd "M-b") 'backward-sentence)
-;; (global-set-key (kbd "M-f") 'forward-sentence)
-
 
 (defvar dvorak-keyswaps
-  '((?\C-h . ?\C-p)
-    (?\C-p . ?\C-h)
-    (?\C-z . ?\C-x)
-    (?\C-x . ?\C-z)
-    (?\C-j . ?\C-c)
-    (?\C-w . ?\C-c)
-    (?\C-c . ?\C-w)))
+  '(("C-h" . "C-p")
+    ("C-p" . "C-h")
+    ("M-p" . "M-h")
+    ("M-h" . "M-p")
+    ("C-M-p" . "C-M-h")
+    ("C-M-h" . "C-M-p")
+    ("C-S-p" . "C-S-h")
+    ("C-S-h" . "C-S-p")
+    ("C-z" . "C-x")
+    ("C-'" . "C-x")
+    ("C-x" . "C-z")
+    ("C-j" . "C-c")
+    ("C-w" . "C-c")
+    ("C-c" . "C-w")))
 
 (defun apply-my-keyswaps ()
   (dolist (key-pair dvorak-keyswaps)
-    (keyboard-translate (car key-pair) (cdr key-pair))))
+    (define-key key-translation-map (kbd (car key-pair)) (kbd (cdr key-pair)))))
 
 (apply-my-keyswaps)
 
@@ -51,35 +45,9 @@
           (lambda (f) (with-selected-frame f
                         (apply-my-keyswaps))))
 
-;;;; Comint Settings
-;;;
-(define-key comint-mode-map (kbd "M-h") 'comint-previous-input)
-(define-key comint-mode-map (kbd "M-p") nil)
-
 ;;;; Org mode Dvorak settings
 ;;;
 (add-hook 'org-mode-hook
           (lambda ()
             (define-key org-mode-map (kbd "M-a") nil)
-            (define-key org-mode-map (kbd "M-e") nil)
-            ;; (define-key org-mode-map (kbd "M-f") 'org-forward-sentence)
-            ;; (define-key org-mode-map (kbd "M-b") 'org-backward-sentence)
-            ))
-
-
-
-;;;; Make the Paredit C-M-p keybinding switch to C-M-h
-(define-key paredit-mode-map (kbd "C-M-h") 'paredit-backward-down)
-(define-key paredit-mode-map (kbd "C-M-p") nil)
-
-
-;;;; Slime mappings
-(add-hook 'slime-mode-hook
-          (lambda ()
-            (define-key slime-mode-map (kbd "M-p") nil)
-            (define-key slime-mode-map (kbd "M-h") 'slime-previous-note)))
-
-(add-hook 'slime-repl-mode-hook
-          (lambda ()
-            (define-key slime-repl-mode-map (kbd "M-h") 'slime-repl-previous-input)
-            (define-key slime-repl-mode-map (kbd "M-p") nil)))
+            (define-key org-mode-map (kbd "M-e") nil)))
