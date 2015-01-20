2dced608a11fcfb70fff37ae338c7d54de2f2161
Apple <Apple@server.fake>
2015-01-20 11:56:19 +0000

    Calling start_slime.sh removes emacs from the pty

--- start_slime.sh
+++ start_slime.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-emacs --no-init-file --no-site-file --eval='(load "~/.emacs.d/slime_only.el")'
+nohup emacs --no-init-file --no-site-file --eval='(load "~/.emacs.d/slime_only.el")' > /dev/null 2>&1 &
+disown
