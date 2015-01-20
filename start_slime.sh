7bac392338bfdd81b1fcecfa81d4460233327001
Apple <Apple@server.fake>
2015-01-20 21:01:28 +0000

    Improve start_slime script (use load not eval)

--- start_slime.sh
+++ start_slime.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup emacs --no-init-file --no-site-file --eval='(load "~/.emacs.d/slime_only.el")' > /dev/null 2>&1 &
+nohup emacs --no-init-file --no-site-file --load ~/.emacs.d/slime_only.el > /dev/null 2>&1 &
 disown
