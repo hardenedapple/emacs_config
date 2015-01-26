cbc2ab804857ff2d6f28549b811ef5e717ff2bef
Apple <Apple@server.fake>
2015-01-26 10:12:40 +0000

    Moved C-x translation back to C-z

--- init.el
+++ init.el
@@ -214,7 +214,10 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+;; Remove face commands, and emacs suspension commands
 (global-unset-key (kbd "M-o"))
+(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x C-z"))
 
 
 ;;;; Whitespace and indent
