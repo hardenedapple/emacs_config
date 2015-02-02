0866aadfef895f479305bb9b4835aba4cbeeebed
Apple <Apple@server.fake>
2015-02-02 17:07:47 +0000

    Set LATEX-RUN-COMMAND to pdflatex

--- plugin_configurations/69_filetype_specific_settings.el
+++ plugin_configurations/69_filetype_specific_settings.el
@@ -16,6 +16,7 @@
 
 ;;;; Latex Settings
 ;;;;
+(setq latex-run-command "pdflatex")
 (add-hook 'latex-mode-hook
           (lambda ()
             (set-fill-column 125)))  ; usually only write latex on large screens
