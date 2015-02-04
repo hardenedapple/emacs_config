4df05c41209d14df52a1ea66c365e9fe47f239f7
Apple <Apple@server.fake>
2015-02-04 08:44:21 +0000

    Don't need to specify the helm-find-file remap

--- plugin_configurations/89_non_evil_plugin_settings.el
+++ plugin_configurations/89_non_evil_plugin_settings.el
@@ -103,7 +103,6 @@
 (global-set-key (kbd "<menu>") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (setq buffer-choose-default-function 'helm-mini)
-(define-key ctl-x-map (kbd "C-f") 'helm-find-files)
 (define-key helm-command-prefix "g" 'helm-do-grep)
 (define-key helm-command-prefix "o" 'helm-occur)
 
