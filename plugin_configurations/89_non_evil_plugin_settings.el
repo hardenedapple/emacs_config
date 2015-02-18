a74651dcb19c4a270b16cad12242b9cc4a7461fe
Apple <Apple@server.fake>
2015-02-18 20:27:53 +0000

    Remove mapping for EXECUTE-EXTENDED-COMMAND as it's on the <menu> key

--- plugin_configurations/89_non_evil_plugin_settings.el
+++ plugin_configurations/89_non_evil_plugin_settings.el
@@ -324,7 +324,6 @@ and run a command given by the user in that window.
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 
 ;;;; Slime Settings
