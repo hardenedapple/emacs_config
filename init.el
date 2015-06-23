f2fff9b4c3a4af32a90ff73c5a8c8db2513de007
hardenedapple <hardenedapple@server.fake>
2015-06-23 21:25:59 +0100

    Remove some stupidity from requiring packages

--- init.el
+++ init.el
@@ -14,44 +14,37 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(let
-    ((download-only '(monokai-theme tangotango-theme))
+(let ((download-only '(monokai-theme tangotango-theme))
+      (elpa-packages '(ace-jump-mode arduino-mode buffer-move elisp-slime-nav
+                                     expand-region goto-chg ido-ubiquitous
+                                     ido-at-point ido-vertical-mode jump-char
+                                     list-register magit monky multiple-cursors
+                                     paredit projectile python-pylint quack
+                                     smart-tab smart-window smex undo-tree
+                                     vimrc-mode window-number wrap-region
+                                     xcscope yasnippet transpose-mark
+                                     markdown-mode
+                                     ;; Occasionally use the below to show
+                                     ;; currently unbound keys, which is useful
+                                     ;; for deciding on a keybinding.
+                                     ;; unbound
+                                     ))
+      (require-only '(desktop dired-x eldoc em-smart eshell
+                              le-eval-and-insert-results nameses transpose-frame
+                              uniquify epa-file)))
+  ;; Install packages, require packages
+  (dolist (p (append elpa-packages download-only))
+    (unless (package-installed-p p)
+      (package-install p)))
 
-     (elpa-packages
-      '(ace-jump-mode arduino-mode buffer-move elisp-slime-nav expand-region
-                      goto-chg ido-ubiquitous ido-at-point ido-vertical-mode
-                      jump-char list-register magit monky multiple-cursors
-                      paredit projectile python-pylint quack smart-tab
-                      smart-window smex undo-tree vimrc-mode
-                      window-number wrap-region xcscope yasnippet
-                      transpose-mark markdown-mode
-                      ;; I occasionally use this, but not usually -- shows currently
-                      ;; unbound keys, which is useful for deciding on a keybinding.
-                      ;; unbound
-                      ))
-
-     (require-only
-      '(desktop dired-x eldoc em-smart eshell le-eval-and-insert-results
-                nameses transpose-frame uniquify epa-file)))
-
-  (let
-      ((require-packages
-        (append require-only elpa-packages)))
-
-    ;; Install packages, require packages
-    (dolist (p (append elpa-packages download-only))
-      (unless (package-installed-p p)
-        (package-install p)))
-
-    (dolist (p require-packages)
-      (require p))))
+  (dolist (p (append require-only elpa-packages))
+    (require p)))
 
 ;;; Load all files in the directory config
 ;;; Name of file denotes order it's loaded in.
 ;;; Note order matters in way here:
 ;;;    wrap-region after paredit to not overwrite '('
-(dolist (conf-file
-         (directory-files "~/.emacs.d/config/" t "^[^.].+\\.elc?$"))
+(dolist (conf-file (directory-files "~/.emacs.d/config/" t "^[^.].+\\.elc?$"))
   (load conf-file))
 
 ;;; Remove some settings from MINOR-MODE-ALIST
