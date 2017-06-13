bb2809dfa1adae8e790724937a1cbc7f8652d255
Matthew Malcomson <hardenedapple@gmail.com>
2017-06-13 23:16:50 +0100

    Remove customisation for the `python-shell'
    Recent updates to ipython mean my customisations stopped working and I got a
    load of control characters around the prompt.
    I gave a little effort to fixing it, but it's much easier to just use the
    default options and the default python version.
    
    Anyway, the only feature I use of IPython is the ability to reference previous
    outputs easily (via Out[<number>]), and I can get by without that.

--- config/79_built_in_packages.el
+++ config/79_built_in_packages.el
@@ -462,18 +462,6 @@ don't have the package installed."
 
 ;;;; Python Settings
 ;;;;
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args ""
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
- "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
- "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
- "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-
 (with-eval-after-load 'python
     (define-key inferior-python-mode-map (kbd "TAB")
       'python-shell-completion-complete-or-indent))
