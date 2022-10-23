003ac931d9464a9de1cb872431b4fbddc7e0d906
Matthew Malcomson <matthew.malcomson@arm.com>
2022-10-23 21:51:27 +0100

    Add key translations for Halmak

--- config/99_alternate_keybindings.el
+++ config/99_alternate_keybindings.el
@@ -36,14 +36,50 @@
     (?\C-j . ?\C-c)
     (?\C-c . ?\C-w)))
 
-(defun apply-my-keyswaps ()
+(defvar halmak-keyswaps
+  '(("M-p" . "M-h")
+    ("M-h" . "M-p")
+    ("C-M-p" . "C-M-h")
+    ("C-M-h" . "C-M-p")
+    ("C-S-p" . "C-S-h")
+    ("C-S-h" . "C-S-p")
+    ("C-'" . "C-v")
+    ;; Just to be able to still use the C-j key
+    ;; It's not available because I map it away below.
+    ("C-c j" . "C-j")))
+
+(defvar halmak-key-translations
+  '((?\C-v . ?\C-x)
+    (?\C-h . ?\C-p)
+    (?\C-p . ?\C-c)
+    (?\C-j . ?\C-h)))
+
+(defun apply-dvorak-keyswaps ()
   (dolist (key-pair dvorak-keyswaps)
     (define-key key-translation-map (kbd (car key-pair)) (kbd (cdr key-pair))))
   (dolist (key-characters dvorak-key-translations)
     (keyboard-translate (car key-characters) (cdr key-characters))))
 
-(apply-my-keyswaps)
+(defun apply-halmak-keyswaps ()
+  ;; We're lucky that the halmak keys I swap here are all *from* the same key.
+  ;; It means there will be no key "left over" mapped to 'nil in our keymap
+  ;; when switching between the two.
+  (dolist (key-pair halmak-keyswaps)
+    (define-key key-translation-map (kbd (car key-pair)) (kbd (cdr key-pair))))
+  (dolist (key-characters halmak-key-translations)
+    (keyboard-translate (car key-characters) (cdr key-characters))))
+
+(cl-defun remove-personal-keyswaps (&optional (translations dvorak-key-translations))
+  (dolist (key-characters translations)
+    (keyboard-translate (car key-characters) 'nil)))
+
+(defun switch-to-halmak ()
+  (interactive)
+  (remove-personal-keyswaps)
+  (apply-halmak-keyswaps))
+
+(apply-dvorak-keyswaps)
 
 (add-hook 'after-make-frame-functions
           (lambda (f) (with-selected-frame f
-                        (apply-my-keyswaps))))
+                        (apply-dvorak-keyswaps))))
