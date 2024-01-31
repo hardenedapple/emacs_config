3572ba7d65cb53ac52d467741482fe5de1a4ac31
Matthew Malcomson <matthew.malcomson@arm.com>
2024-01-31 21:52:27 +0000

    Add some description of the major-mode
    Make a start towards describing what the point of this major-mode is
    for to others.

--- packages/vsh.el
+++ packages/vsh.el
@@ -1,11 +1,124 @@
-;;; vsh.el --- specialized and modified comint.el for shell -*- lexical-binding: t -*-
+;;; vsh.el --- Alternate PTY interface for complex terminal sessions -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2024 Matthew Malcomson
 
 ;; Author: Matthew Malcomson <hardenedapple@gmail.com>
+;; Version: 1.0
 ;; Keywords: processes
+;; URL: https://github.com/hardenedapple/vsh
 
 ;;; Commentary:
+
+;; This package provides a major mode for running terminal sessions.
+;; When running experimental terminal sessions (where one does not know exactly
+;; how to do what they are attempting) one can find themselves repeating parts
+;; of that session multiple times with slight variation.
+;;
+;; Similarly such repetition with slight variation could happen when performing
+;; actions that could be scripted, but where the percieved benefit of scripting
+;; is not quite enough for the current task (e.g. because it's too small a
+;; task).
+;;
+;; Both of the above situations can benefit from some kind of automation "in
+;; between" scripting and typing into the terminal.  That is where this package
+;; comes in.
+;;
+;; While running a terminal session in a `vsh' buffer one is automatically
+;; recording a list of commands that got run (as in history).  One can edit the
+;; output as per a `shell' buffer etc.  The difference with `vsh' is that you
+;; can also remove commands that were mistakes, and you can run previous
+;; commands in place (and as a block).
+;; As an example, when running in a GDB session one might have a session with
+;; various mistakes and temporary commands in the history (simplistic example
+;; below):
+;;
+;; vshcmd: > gdb ./test
+;; ...
+;; vshcmd: > break printf
+;; ...
+;; vshcmd: > y
+;; ...
+;; vshcmd: > run
+;; ...
+;; vshcmd: > # To represent bunch of different commands that are not "important".
+;; vshcmd: > apropos shared
+;; ...
+;; vshcmd: > # Notice that we should have put a breakpoint on `puts' rather than `printf'.
+;; vshcmd: > disassemble main
+;; Dump of assembler code for function main:
+;;    0x0000555555555149 <+0>:	endbr64
+;;    0x000055555555514d <+4>:	push   %rbp
+;;    0x000055555555514e <+5>:	mov    %rsp,%rbp
+;;    0x0000555555555151 <+8>:	lea    0xeac(%rip),%rax        # 0x555555556004
+;;    0x0000555555555158 <+15>:	mov    %rax,%rdi
+;;    0x000055555555515b <+18>:	call   0x555555555050 <puts@plt>
+;;    0x0000555555555160 <+23>:	mov    $0x0,%eax
+;;    0x0000555555555165 <+28>:	pop    %rbp
+;;    0x0000555555555166 <+29>:	ret
+;; End of assembler dump.
+;; (gdb)
+;; vshcmd: > break puts
+;; ...
+;; vshcmd: > delete 1
+;; ...
+;; vshcmd: > run
+;; Starting program: /home/matmal01/test
+;; [Thread debugging using libthread_db enabled]
+;; Using host libthread_db library "/lib/x86_64-linux-gnu/libthread_db.so.1".
+;;
+;; Breakpoint 2, 0x00007ffff7dfbe50 in puts () from /lib/x86_64-linux-gnu/libc.so.6
+;; (gdb)
+;;
+;; Imagine that the day after you come back to the same debugging session.
+;; First off running inside `vsh-mode' has recorded the session so you don't
+;; have to remember what you did.  Secondly you can simply clean up the session
+;; in the buffer before running it so you don't make the same mistakes as last
+;; time.
+;;
+;; vshcmd: > gdb ./test
+;; ...
+;; vshcmd: > break puts
+;; ...
+;; vshcmd: > run
+;; ...
+;;
+;; Furthermore, you can put all these commands in one block and run the entire
+;; block every time you start GDB (imagine there are three or four setup
+;; commands that via trial and error you have found you need to run in these
+;; debugging sessions).
+;;
+;; vshcmd: > gdb ./test
+;; vshcmd: > break puts
+;; vshcmd: > run
+;; ... ... ...
+;;
+;; Some time later on, if you perform this same kind of debugging session
+;; multiple times, you might save this into a GDB command or setup file.
+;; The main concept here is that `vsh-mode' provides a very low-barrier
+;; progression from doing something complicated to having it partially
+;; automated.  This partially automated state is often good enough for many
+;; tasks, but for a task that needs to be properly automated this partially
+;; automated state is also having done part of the work towards automation.
+;;
+;; Anecdotally I can also mention that knowing complex commands will stay around
+;; ready for me to use again (and easily editable with standard text editor
+;; commands to adjust for future use) has meant that I much more often write
+;; complex commands to do what I want in one step rather than simpler commands
+;; that take a few manual steps.
+;;
+;;
+;; All this is done by breaking the fundamental assumption of terminals that the
+;; command and output are all appended to previous text.  This comes with a few
+;; quirks (like when looking at the `vsh-mode' buffer you don't immediately know
+;; where the last command has come from, nor whether you're currently running a
+;; python REPL, gdb session, or shell session).  Most of these quirks have not
+;; turned out to be problematic enough to compare to the benefits listed above.
+;;
+;; N.b. another use for this mode is to prepare live demonstrations.  You can
+;; test the exact terminal session you intend to run and avoid typos during the
+;; demonstration while still allowing the ability to do something slightly
+;; different on the day.
+
 
 ;;; Customization and Buffer Variables
 
@@ -52,8 +165,7 @@ SGR control sequences are interpreted as determining color and *not* filtered."
 ;; silly for the sole purpose of having a separate emacs server process.
 ;;
 ;;
-;;
-;; There are two approaches that we implement:
+;; There are two approaches I thought about:
 ;; 1) When starting a VSH process for the first time, start a special VSH server
 ;;    for all vsh helpers to communicate with.  This server is started when needed,
 ;;    and is less likely to be stopped without knowing that it disables VSH
