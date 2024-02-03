16998992868ec1c64f7902750434b77470ae2585
Matthew Malcomson <matthew.malcomson@arm.com>
2024-02-03 23:44:30 +0000

    Add some tests for `vsh-mode'
    Still many to go, but this at least tests enough to catch the two
    errors I already know are in my code.

--- packages/vsh.el
+++ packages/vsh.el
@@ -1096,3 +1096,65 @@ Entry to this mode runs the hooks on `vsh-mode-hook'."
 (provide 'vsh)
 
 ;;; vsh.el ends here
+
+(defmacro vsh--test-on-linespecs (&rest body)
+  "Run `body' for each point in each line given in `vsh--internal-testing-lines'.
+
+Inserts the following local variables in the scope for `body' to use:
+- `promptend-offset': Index into line where the prompt ends.
+- `line': The current line that `body' is running on.
+- `no-whitespace-offset': Index of line where prompt without whitespace starts.
+- `reset-test': Function to reset buffer text to current line only.
+- `idx': Index into line that point was before body."
+  (let ((linespec (make-symbol "linespec"))
+        (idx (make-symbol "idx")))
+    `(with-temp-buffer
+       (dolist (,linespec vsh--internal-testing-lines)
+         (erase-buffer)
+         (let ((promptend-offset (car ,linespec))
+               (no-whitespace-offset (cadr ,linespec))
+               (line (caddr ,linespec)))
+           (insert line)
+           (dotimes (idx (point-max))
+             (let ((,idx (1+ idx)))
+               (cl-flet ((reset-test ()
+                           (erase-buffer)
+                           (insert line)
+                           (goto-char idx)))
+                 (reset-test)
+                 ,@body))))))))
+
+(defvar vsh--internal-testing-lines
+  '((0 0 "Test output line")
+    (10 10 "vshcmd: > command no space")
+    (12 10 "vshcmd: >   command with space")
+    (0  0 "vshcmd: >  # space before hash, only a split marker")
+    (12 12 "vshcmd: > # comment no space")
+    (22 22 "vshcmd: > # vshcmd: > deactivated command")
+    (23 22 "vshcmd: > # vshcmd: >  deactivated with space")
+    (0 0 "vshcmd: >")                   ; Split that is not a motion marker.
+    (10 10 "vshcmd: > ")                 ; Command line no text.
+    ))
+
+(ert-deftest vsh-bol-test ()
+  "Testing `vsh-bol' always moves cursor to relevant position."
+  (vsh--test-on-linespecs
+   (vsh-bol)
+   (should (eq (point) (1+ promptend-offset)))))
+
+(ert-deftest vsh-nop-discard-test ()
+  "Testing `vsh-line-discard' for those positions where it is a nop."
+  (vsh--test-on-linespecs
+   (cl-flet ((tester (loc index text)
+               (if (<= index loc)
+                   (should (equal (buffer-string) text))
+                 (should (equal (buffer-string)
+                                (string-join
+                                 (list (substring text 0 loc)
+                                       (substring text (1- index)))))))))
+     (vsh-line-discard nil)
+     (tester promptend-offset idx line)
+     (reset-test)
+     (vsh-line-discard t)
+     (tester no-whitespace-offset idx line)
+     (reset-test))))
