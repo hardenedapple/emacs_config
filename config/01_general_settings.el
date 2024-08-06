3dad6ec23d191e4c01c533925715ce7c7851ddc0
Matthew Malcomson <mmalcomson@nvidia.com>
2024-08-06 16:31:54 +0100

    Use `modify-all-frames-parameters' for setting font
    This avoids overriding settings that disabling `scroll-bar-mode'
    configures.  Those settings were meaning that new frames would include
    a scroll bar.
    
    N.b. this change is suggested in the help text for
    `default-frame-alist'.

--- config/01_general_settings.el
+++ config/01_general_settings.el
@@ -572,12 +572,12 @@ e.g. ((\"interactive\" \"Iswapped\")(\"concat\" \"Cswapped\"))
 
 (defun adjust-for-home-monitor ()
   (set-frame-font "Liberation Mono 8")
-  (setq default-frame-alist '((font . "Liberation Mono 8"))))
+  (modify-all-frames-parameters '((font . "Liberation Mono 8"))))
 ;; This changes depending on the monitor I get given, but don't expect to change
 ;; for a while so we should be fine for the moment.
 (defun adjust-for-work-monitor ()
   (set-frame-font "Liberation Mono 11")
-  (setq default-frame-alist '((font . "Liberation Mono 11"))))
+  (modify-all-frames-parameters '((font . "Liberation Mono 11"))))
 ;; N.b. Setting default for in the office (making a guess where is the most
 ;; important).
 (adjust-for-work-monitor)
